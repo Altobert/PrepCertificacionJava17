dcb4c3ba2d222e5bf917e6bb143ca8882757a81e
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-05-30 16:05:44 -0400

    se agregan comentarios

--- src/main/java/cl/asm/java17/Zoo.java
+++ src/main/java/cl/asm/java17/Zoo.java
@@ -2,12 +2,10 @@ package cl.asm.java17;
 
 public class Zoo {
     public static void main(String[] args) {
-        java.util.logging.Logger log = java.util.logging.Logger.getLogger(Zoo.class.getName());
-        log.info("Bienvenido al Zoo!");
+        java.util.logging.Logger log = java.util.logging.Logger.getLogger(Zoo.class.getName());                
+        for(int i=0; i<args.length; i++) {
+           log.info(args[i]);             
+        }
         
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        
-        // Puedes agregar más lógica aquí para manejar otros animales o interacciones
     }
 }
