1b53799e566fbf83f860952621d36b6dbd269116
Alberto Carlos Michell San Martin Concha <alberto.sanmartin@sii.cl>
2025-06-04 11:30:39 -0400

    se agregan comentarios respecto a los packetes

--- src/main/java/cl/asm/java17/Animal.java
+++ src/main/java/cl/asm/java17/Animal.java
@@ -1,5 +1,7 @@
 package cl.asm.java17;
 
+// El nombre publico de la clase tiene que hacer match con el nombre del archivo
+// por lo que si la clase se llama Animal, el archivo debe llamarse Animal.java
 public class Animal {
 
     String name;
@@ -18,6 +20,7 @@ public class Animal {
 //pero no es una buena práctica. EL nombre de la clase interna
 //debe ser diferente al de la clase externa.
 //El nombre del archivo.java debe ser el mismo que la clase public.
+//
 class Animal2{
 
 }
